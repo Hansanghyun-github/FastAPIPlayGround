077ee877ed6189f204f69d5ac375e5af7b120194
HSH <han41562@gmail.com>
2025-01-08 11:07:02 +0000

    feat: add fastapi-health library dependency

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from fastapi_health import health
 from elasticsearch_routes.elasticsearch_router import ElasticsearchRouter
 
+def is_healthy():
+    return {"database": "up", "cache": "up"}
+
 app = FastAPI()
+app.add_api_route("/health", health([is_healthy]))
 
 app.include_router(ElasticsearchRouter)
 
